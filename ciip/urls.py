5e495f8c4f0bafefb9ddae830c61b4f10ee2e303
fraferra <fraferra@cisco.com>
2013-10-08 14:15:24 -0700

    hry

--- ciip/urls.py
+++ ciip/urls.py
@@ -13,11 +13,7 @@ urlpatterns = patterns('',
     url(r'^home/$', views.home, name='home'),
     url(r'^eligibility/$', views.eligibility, name='eligibility'),
     #url(r'^upload_file/$', views.upload_file, name='upload_file'),
-    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
-    (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^reset/(?P[0-9A-Za-z]+)-(?P.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
-   # (r'^admin/(.*)', admin.site.root),
+   
 
 )
 
