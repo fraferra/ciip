801ff3da2e1cdf24e03ad98163adc0efd536078a
fraferra <fraferra@cisco.com>
2013-10-15 10:47:42 -0700

    h

--- mysite/s3utils.py
+++ mysite/s3utils.py
@@ -1,6 +1,7 @@
 
 
 from storages.backends.s3boto import S3BotoStorage
-
+AWS_ACCESS_KEY_ID='AKIAIKGQ5IBXP5TT4ZBA'
+AWS_SECRET_ACCESS_KEY='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'
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
