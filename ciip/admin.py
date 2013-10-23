96d817e29b887e7189ac683142f0b9a16ec1295d
fraferra <fraferra@cisco.com>
2013-10-22 22:23:29 -0700

    d

--- ciip/admin.py
+++ ciip/admin.py
@@ -26,7 +26,7 @@ class CiipAdmin(admin.ModelAdmin):
     #readonly_fields=['gender','first_name','last_name','passport_number','birth_date_day','birth_date_month','birth_date_year','email','phone','address_line1','address_line2','city','zip_code','country']
     readonly_fields=['gender','first_name','last_name','passport_number','birth_date_day','birth_date_month','birth_date_year','email','phone','address_line1','address_line2','city','zip_code','country','university','year_of_graduation','degree','average','question_1', 'question_2','file_name']
     list_display = ('first_name', 'last_name','status','university','user_email')
-    list_filter = ['last_name']
+    list_filter = ['university', 'status']
 
 
     def user_email(self, instance):
