5e4859acd87e6c00efa47903cb2078445501088f
fraferra <fraferra@cisco.com>
2014-06-03 18:35:54 -0700

    cloud database

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ if env =="local":
         }
     }
 
-if env =='cloud':
+elif env =='cloud':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
